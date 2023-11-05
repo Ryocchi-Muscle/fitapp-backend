c8cf6f9ebbaa1d59a946bb62323428faf88e5bd4
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-05 13:50:21 +0900

    application_controllerを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::API
   #SetUserByTokenという拡張機能を使える様になる→Cookie や CORS の設定ができるようになる
   include DeviseTokenAuth::Concerns::SetUserByToken
-
-  #   def some_action
-  #     @data = "Some data"
-  #     redirect_to "https://google.com?account_confirmation_success=true", allow_other_host: true
-  #   end
-
+  
 end
