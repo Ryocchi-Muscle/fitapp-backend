b931584fcd8badcafdd7628edb547c4e3c6759e1
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-11 01:11:55 +0900

    fix routes.rb

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -8,22 +8,11 @@ class Auth::SessionsController < DeviseTokenAuth::SessionsController
     end
   end
 
-  # def guest_sign_in
-  #   user = User.guest
-  #     user.password = SecureRandom.urlsafe_base64
-  #     user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-  #   end
-  #   sign_in user
-  #   flash[:notice] = "ゲストユーザーとしてログインしました。"
-  #   redirect_to root_path
-  # end
+  def guest_sign_in
+    # ゲストユーザーでログイン
+    @resource = User.guest # ①ゲストユーザー情報を取得
+    @token = @resource.create_token # ②ゲストユーザーの認証情報を作成
+    @resource.save! # ③認証情報を保存
+    render_create_success # ④認証情報を持つゲストログインユーザー情報を返す
+  end
 end
-
-# class Auth::SessionsController < DeviseTokenAuth::SessionsController
-#   def guest_sign_in
-#     @resource = User.guest          #ゲストユーザーを取得
-#     @token = @resource.create_token  #認証情報作成
-#     @resource.save!                  #認証情報保続
-#     render_create_success           #認証情報を持つゲストログインユーザー情報を返す
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # ログイン機能のルーティング
   mount_devise_token_auth_for "User", at: "auth", controllers: {
                                         registrations: "auth/registrations",
+                                        sessions: "auth/sessions",
                                       }
   # ログインユーザー取得のルーティング
   namespace :auth do
-    resources :sessions, only: %i[index]
+    resources :sessions, only: %i[:index]
   end
 
   # ゲストログイン機能
