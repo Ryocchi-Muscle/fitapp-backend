b83553882e439f8a55857f5e90cfeea89e4e2a26
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-13 19:25:09 +0900

    routes.rbを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # ログイン機能のルーティング
   mount_devise_token_auth_for "User", at: "auth", controllers: {
-    registrations: "auth/registrations",
-  }
+            registrations: "auth/registrations",
+          }
   # ログインユーザー取得のルーティング
   namespace :auth do
     resources :sessions, only: [:index]
@@ -11,5 +11,11 @@ Rails.application.routes.draw do
   # ゲストログイン機能
   devise_scope :user do
     post "auth/guest_sign_in", to: "auth/guest_sessions#guest_sign_in"
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get "health_check", to: "health_check#index"
+    end
   end
 end
