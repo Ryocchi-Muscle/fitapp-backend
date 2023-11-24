e586d9ea585d805cbf9f84bf17ecb1258465c8cf
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-24 20:25:00 +0900

    Rails側のポート番号変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.default_options = { from: ENV["EMAIL_ADDRESS"] }
   # hostにはデフォルトでlocalhost3000になっているので、Railsのポート番号である3001に変更する。
-  config.action_mailer.default_url_options = { host: "localhost:3001" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
